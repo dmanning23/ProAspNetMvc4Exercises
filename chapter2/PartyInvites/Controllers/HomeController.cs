efb470fc2e734aae36877e44eeac6c6949f55f8a
Daniel Manning <daniel.manning@demandmedia.com>
2014-03-20 18:05:39 -0500

    master: a-0 ch2 commit

---
+++ chapter2/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+			int hour = DateTime.Now.Hour;
+			ViewBag.Greeting = ((hour < 12) ? "Good morning" : "Good afternoon");
+			return View();
+        }
+
+		[HttpGet]
+		public ViewResult RsvpForm()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ViewResult RsvpForm(GuestResponse response)
+		{
+			return View("Thanks", response);
+		}
+
+    }
+}
